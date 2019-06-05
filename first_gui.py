e26b6cbb4b7a5860a57d0cd637ee79166a07c26d
Caden <jarrardtiger227@yahoo.com>
2019-06-05 16:23:01 -0400

    Testing new calculator design

--- first_gui.py
+++ first_gui.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 win = tk.Tk()
 
-win.geometry('250x450')
+win.geometry('220x350')
+
+win.resizable(False, False)
 
 win.title('First GUI')
 
@@ -12,60 +14,73 @@ lab1.pack()
 lab2 = tk.Label(win, text='Second Label')
 lab2.pack()
 
-operFrame = tk.Frame(win)
-operFrame.pack(side='right')
-
-# Create the Operator Buttons
-but1 = tk.Button(operFrame, text='C').pack()
-but2 = tk.Button(operFrame, text='+').pack()
-but3 = tk.Button(operFrame, text='-').pack()
-but4 = tk.Button(operFrame, text='=').pack()
 
 # Create the Number Buttons
 numFrame1 = tk.Frame(win)
 numFrame1.pack(side='left')
 
 numFrame2 = tk.Frame(win)
-numFrame2.pack(side='bottom')
+numFrame2.pack(side='left')
 
 numFrame3 = tk.Frame(win)
-numFrame3.pack(side='right')
+numFrame3.pack(side='left')
 
 num7 = tk.Button(numFrame1, text='7')
-num4 = tk.Button(numFrame1, text='4')
+num5 = tk.Button(numFrame1, text='5')
 num1 = tk.Button(numFrame1, text='1')
 
-num7.config(height = 4, width = 4)
-num4.config(height = 4, width = 4)
-num1.config(height = 4, width = 4)
+num7.config(height = 5, width = 6)
+num5.config(height = 5, width = 6)
+num1.config(height = 5, width = 6)
 
-num7.pack()
-num4.pack()
-num1.pack()
+num7.grid()
+num5.grid()
+num1.grid()
 
 num8 = tk.Button(numFrame2, text='8')
 num5 = tk.Button(numFrame2, text='5')
 num2 = tk.Button(numFrame2, text='2')
 
-num8.config(height = 4, width = 4)
-num5.config(height = 4, width = 4)
-num2.config(height = 4, width = 4)
+num8.config(height = 5, width = 6)
+num5.config(height = 5, width = 6)
+num2.config(height = 5, width = 6)
 
-num8.pack()
-num5.pack()
-num2.pack()
+num8.grid(column=1)
+num5.grid(column=1)
+num2.grid(column=1)
 
 num9 = tk.Button(numFrame3, text='9')
 num6 = tk.Button(numFrame3, text='6')
 num3 = tk.Button(numFrame3, text='3')
 
-num9.config(height = 4, width = 4)
-num6.config(height = 4, width = 4)
-num3.config(height = 4, width = 4)
+num9.config(height = 5, width = 6)
+num6.config(height = 5, width = 6)
+num3.config(height = 5, width = 6)
 
-num9.pack()
-num6.pack()
-num3.pack()
+num9.grid(column=2)
+num6.grid(column=2)
+num3.grid(column=2)
+
+
+# Create the Operator Buttons
+operFrame = tk.Frame(win)
+operFrame.pack(side='left')
+
+
+but1 = tk.Button(operFrame, text='C')
+but2 = tk.Button(operFrame, text='+')
+but3 = tk.Button(operFrame, text='-')
+but5 = tk.Button(operFrame, text='=')
+
+but1.config(height=4, width=7)
+but2.config(height=3, width=7)
+but3.config(height=3, width=7)
+but5.config(height=5, width=7)
+
+but1.grid(column=3)
+but2.grid(column=3)
+but3.grid(column=3)
+but5.grid(column=3)
 
 
 win.mainloop()
